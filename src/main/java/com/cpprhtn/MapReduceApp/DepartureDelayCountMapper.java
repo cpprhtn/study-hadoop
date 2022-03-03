5ce2958d503b26971c5b00361170bafe51766519
cpprhtn <cpprhtn@naver.com>
2022-03-03 13:43:48 +0900

    매퍼구현

---
+++ src/main/java/com/cpprhtn/MapReduceApp/DepartureDelayCountMapper.java
@@ -0,0 +1,32 @@
+package com.cpprhtn.MapReduceApp;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import com.cpprhtn.common.AirlinePerformanceParser;
+
+import java.io.IOException;
+
+public class DepartureDelayCountMapper extends
+  Mapper<LongWritable, Text, Text, IntWritable> {
+
+  // map 출력값
+  private final static IntWritable outputValue = new IntWritable(1);
+  // map 출력키
+  private Text outputKey = new Text();
+
+  public void map(LongWritable key, Text value, Context context)
+    throws IOException, InterruptedException {
+
+    AirlinePerformanceParser parser = new AirlinePerformanceParser(value);
+
+    // 출력키 설정
+    outputKey.set(parser.getYear() + "," + parser.getMonth());
+
+    if (parser.getDepartureDelayTime() > 0) {
+      // 출력 데이터 생성
+      context.write(outputKey, outputValue);
+    }
+  }
+}

7e51a1da332a34b2ffb8580c132ce24abcd71a15
cpprhtn <cpprhtn@naver.com>
2022-03-10 15:53:49 +0900

    package path 변경

--- src/main/java/com/cpprhtn/MapReduceApp/ArrivalDelayCount.java
+++ src/main/java/com/cpprhtn/MapReduceApp/ArrivalDelayCount.java
@@ -1,4 +1,4 @@
-package com.cpprhtn.MapreduceApp;
+package com.cpprhtn.MapReduceApp;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
--- src/main/java/com/cpprhtn/MapReduceApp/ArrivalDelayCountMapper.java
+++ src/main/java/com/cpprhtn/MapReduceApp/ArrivalDelayCountMapper.java
@@ -1,4 +1,4 @@
-package com.cpprhtn.MapreduceApp;
+package com.cpprhtn.MapReduceApp;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
